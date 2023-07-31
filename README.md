848e236c1ea2081605c169eb82bc910c2cff6e3f
tomiaa <1178852449@qq.com>
2023-07-31 17:39:56 +0800

    feat: init

---
+++ README.md
@@ -0,0 +1,5 @@
+# vue 纯前端 预览 word 文件 docx
+
+## 文档地址
+
+https://kuangyx.cn/docs/%E6%96%87%E7%AB%A0/%E5%89%8D%E7%AB%AF/vue%E9%A2%84%E8%A7%88word%E6%96%87%E4%BB%B6docx.html#%E5%AE%89%E8%A3%85-npm-%E4%BE%9D%E8%B5%96
---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="my-component" ref="preview">
+    <input type="file" @change="preview" ref="file">
+  </div>
+</template>
+<script>
+window.JSZip = require('jszip') 
+const docx = require('docx-preview'); // npm i docx-preview@0.1.4
+export default {
+  methods:{
+    preview(e){
+      docx.renderAsync(this.$refs.file.files[0],this.$refs.preview) // 渲染到页面预览
+    }
+  }
+};
+</script>
+<style lang="less" scoped>
+.my-component{
+  width: 100%;
+  height: 90vh;
+  border: 1px solid #000;
+}
+</style>
